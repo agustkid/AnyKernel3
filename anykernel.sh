a469a735fcd1679fe4833b8ecb9acedb2ef62c42
agustkid <agustkid@gmail.com>
2021-03-07 17:09:42 +0000

    Wellcome to Kirik

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Kernel builds by ghostrider-reborn
+kernel.string=Kirik Kernel by Agus
 do.devicecheck=1
 do.modules=0
 do.systemless=1
